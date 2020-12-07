382beb8a6e5de773920a57a73fbc6c7be5c09cc0
Abhijeet Bhagat <bhagat.abhijeet@gmail.com>
2020-12-06 22:43:58 -0800

    complete homepage and nav.jsx

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,23 +1,56 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import book from "../assets/book.png";
+import Link from "@material-ui/core/Link";
 
-function Nav() {
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    backgroundColor: "#2ba2ff",
+    color: "#ffffff",
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+export default function Nav() {
+  const classes = useStyles();
+
   return (
+    <div className={classes.root}>
+      <AppBar position="static" color="transparent">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+          >
+            <Link href="/">
+              <img src={book} alt={book} height={40} />
+            </Link>
+          </IconButton>
 
-    <nav className="navbar navbar-expand-lg navbar-light" >
-      <div className = "container">
-      <Link className="navbar-brand" to="/">Google Books</Link>
-      <ul className="navbar-nav ml-auto">
-          <li className="nav-item ">
-            <Link className="nav-link" to="/search">Search</Link>
-          </li>
-          <li className="nav-item ">
-            <Link className="nav-link" to="/saved">Saved</Link>
-          </li>
-        </ul>
-      </div>
-    </nav>
+          <Typography variant="h6" className={classes.title}>
+            Google Books Search
+          </Typography>
+          <Button color="inherit" href="/search">
+            Search
+          </Button>
+          <Button color="inherit" href="/saved">
+            Saved
+          </Button>
+        </Toolbar>
+      </AppBar>
+    </div>
   );
 }
-
-export default Nav;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,19 +1,122 @@
+import Container from "@material-ui/core/Container";
 import React from "react";
-import logo from '../assets/logo.svg';
+import { Spring } from "react-spring/renderprops";
+import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
+import Paper from "@material-ui/core/Paper";
+import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import SearchIcon from "@material-ui/icons/Search";
+import FindInPageIcon from "@material-ui/icons/FindInPage";
 
-function Home () {
+const useStyles = makeStyles((theme) => ({
+  root: {},
+  box: {
+    backgroundColor: "#2ba2ff",
+    height: 200,
+    margin: 30,
+    position: "relative",
+    borderRadius: 30,
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    color: "#ffffff",
+  },
+  paper: {
+    display: "flex",
+    flexDirection: "row",
+    flexWrap: "wrap",
+    alignItems: "center",
+    justifyContent: "space-evenly",
+    margin: 40,
+    padding: 40,
+  },
+}));
 
-    return (
-        <div className ="container main">
-            <div className = "row">
-                <div className= "col welcomeColumn">
-                <h5 id="welcomeHeader">Welcome to Google Books Search! The name speaks for itself..</h5>
+function Home() {
+  const classes = useStyles();
+  return (
+    <React.Fragment>
+      <Container>
+        <Spring
+          config={{ tension: 280, friction: 60, duration: 5000 }}
+          from={{ color: "#fff" }}
+          to={{ color: "black" }}
+        >
+          {(props) => (
+            <Typography
+              component="h1"
+              variant="h5"
+              style={{ textAlign: "center", ...props }}
+            >
+              Google Books Search!
+            </Typography>
+          )}
+        </Spring>
 
-                <img src={logo} className="App-logo" alt="logo" />
-                </div>
-            </div>
-        </div>
-    )
+        <Paper className={classes.paper}>
+          <Grid item xs={4}>
+            <Spring
+              config={{ tension: 120, friction: 14 }}
+              from={{
+                opacity: 0,
+                left: -500,
+                top: -200,
+                transform: "rotate(0deg)",
+              }}
+              to={{ opacity: 1, left: 0, top: 0, transform: "rotate(360deg)" }}
+            >
+              {(props) => (
+                <Box className={classes.box} style={props}>
+                  <Button
+                    href="/search"
+                    size="large"
+                    startIcon={<SearchIcon />}
+                    variant="contained"
+                    color="primary dark"
+                  >
+                    Search
+                  </Button>
+                </Box>
+              )}
+            </Spring>
+          </Grid>
+          <Grid item xs={4}>
+            <Spring
+              from={{
+                opacity: 0,
+                right: -500,
+                top: -200,
+                transform: "rotate(0deg)",
+              }}
+              to={{
+                opacity: 1,
+                right: 0,
+                top: 0,
+                transform: "rotate(-360deg)",
+              }}
+            >
+              {(props) => (
+                <Box className={classes.box} style={props}>
+                  <Button
+                    href="/saved"
+                    size="large"
+                    endIcon={<FindInPageIcon />}
+                    variant="contained"
+                    color="secondary light"
+                  >
+                    Saved Search
+                  </Button>
+                </Box>
+              )}
+            </Spring>
+          </Grid>
+        </Paper>
+      </Container>
+    </React.Fragment>
+  );
 }
 
 export default Home;
